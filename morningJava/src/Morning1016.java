fff0f822f69c6d03eb1431d3983cc6536d81d79b
kawksh <duqrlwjstk@naver.com>
2020-10-16 10:47:38 +0900

    first commit

---
+++ morningJava/src/Morning1016.java
@@ -0,0 +1,22 @@
+public class Morning1016{
+    public static void main(String[] args){
+        System.out.println("Java Sample");
+        
+        int[][] regAry = new int[5][5];
+        int cnt = 0;
+        int cnt2;
+
+        for(int i = 0 ; i<5; i++){
+            cnt++;
+            cnt2 = cnt;
+            for(int j =0; j<5; j++){                
+                regAry[i][j] = cnt2;
+                cnt2+=5;
+                System.out.printf("%3d",regAry[i][j]);
+            }
+            System.out.println("");
+        }
+
+        
+    }
+}
---
+++ morningJava/src/Test.java
@@ -0,0 +1,25 @@
+public class Test{
+    public static void main(String[] args){
+        System.out.println("Java Sample");
+        
+        int[][] regAry = new int[5][5];
+        int cnt = 1;
+
+        for(int i = 0 ; i<5; i++){
+            
+            for(int j =0; j<5; j++){                
+                regAry[i][j] = cnt++;
+            }
+        }
+
+        for(int i = 0 ; i<5; i++){
+            
+            for(int j =0; j<5; j++){        
+                System.out.printf("%3d",regAry[j][i]);
+            }
+            System.out.println("");
+        }
+
+        
+    }
+}
