3ce59514f9b71002abd3fee73a30c4d78ae8df72
kawksh <duqrlwjstk@naver.com>
2020-10-26 17:51:05 +0900

    학원 10/26

---
+++ js/dom.js
@@ -0,0 +1,7 @@
+console.log(this);
+// let text = this.document.children[0].innerHTML += "<div></div>";
+// text = this.document.children[0].children[1].innerHTML;
+console.log(text);
+
+// let sel = document.querySelector("body").innerHTML;
+console.log(sel);
